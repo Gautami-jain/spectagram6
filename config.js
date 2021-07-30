46ada8752e6e43a469ff99ed2e42dd0948baf61f
Gautami-jain <76868217+Gautami-jain@users.noreply.github.com>
2021-07-30 21:29:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCgEkbTAYdYaXM8AjryNTdgqzE-RDAPVLI",
+    authDomain: "spectagram-28bda.firebaseapp.com",
+    projectId: "spectagram-28bda",
+    storageBucket: "spectagram-28bda.appspot.com",
+    messagingSenderId: "244263228162",
+    appId: "1:244263228162:web:dbaaea73810bf69dc6a81b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
